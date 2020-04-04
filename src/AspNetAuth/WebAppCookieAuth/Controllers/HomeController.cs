811a4edb4a1c8748044a11b1ec274d216f459ba5
Peter <sheraium@gmail.com>
2020-04-04 23:02:10 +0800

    add cookie auth sample

--- src/AspNetAuth/WebAppCookieAuth/Controllers/HomeController.cs
+++ src/AspNetAuth/WebAppCookieAuth/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebAppCookieAuth.Filter;
 
 namespace WebAppCookieAuth.Controllers
 {
@@ -25,6 +26,40 @@ namespace WebAppCookieAuth.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        public ActionResult Login(string account, string password)
+        {
+            var cookieName = "mvcAuth";
+            if (account == "mvc" && password == "123456")
+            {
+                if (Response.Cookies.AllKeys.Contains(cookieName))
+                {
+                    var cookieVal = Response.Cookies[cookieName].Value;
+                    HttpContext.Application.Remove(cookieVal);
+
+                    Response.Cookies.Remove(cookieName);
+                }
+
+                var token = Guid.NewGuid().ToString();
+
+                HttpContext.Application[token] = DateTime.UtcNow.AddHours(1);
+
+                var hc = new HttpCookie(cookieName, token)
+                {
+                    Expires = DateTime.Now.AddHours(1),
+                    HttpOnly = true,
+                };
+
+                Response.Cookies.Add(hc);
+            }
+            return RedirectToAction("Index");
+        }
+
+        [AuthorizePlus]
+        public ActionResult Backend()
+        {
+            return Content("您已進入後台");
         }
     }
 }
---
+++ src/AspNetAuth/WebAppCookieAuth/Filter/AuthorizePlusAttribute.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace WebAppCookieAuth.Filter
+{
+    public class AuthorizePlusAttribute : AuthorizeAttribute
+    {
+        public override void OnAuthorization(AuthorizationContext filterContext)
+        {
+            var token = Convert.ToString(filterContext.HttpContext.Request.Cookies["mvcAuth"].Value);
+
+            if (string.IsNullOrWhiteSpace(token))
+            {
+                base.HandleUnauthorizedRequest(filterContext);
+            }
+
+            var loginTime = Convert.ToDateTime(filterContext.HttpContext.Application[token]);
+
+            if (loginTime > DateTime.UtcNow)
+            {
+                // 驗證通過
+            }
+            else
+            {
+                base.HandleUnauthorizedRequest(filterContext);
+            }
+        }
+    }
+}
