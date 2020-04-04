9c7e601196872e194bfb91728b951aa69f5ff4ac
Peter <sheraium@gmail.com>
2020-04-04 22:41:08 +0800

    add session auth sample

--- src/AspNetAuth/WebAppSessionAuth/Controllers/HomeController.cs
+++ src/AspNetAuth/WebAppSessionAuth/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebAppSessionAuth.Filters;
 
 namespace WebAppSessionAuth.Controllers
 {
@@ -25,6 +26,24 @@ namespace WebAppSessionAuth.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        public ActionResult Login()
+        {
+            Session["auth"] = true;
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Logout()
+        {
+            Session["auth"] = false;
+            return RedirectToAction("Index");
+        }
+
+        [AuthorizePlus]
+        public ActionResult Backend()
+        {
+            return Content("您已進入後台");
         }
     }
 }
---
+++ src/AspNetAuth/WebAppSessionAuth/Filters/AuthorizePlusAttribute.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace WebAppSessionAuth.Filters
+{
+    public class AuthorizePlusAttribute : AuthorizeAttribute
+    {
+        public override void OnAuthorization(AuthorizationContext filterContext)
+        {
+            if (Convert.ToBoolean(filterContext.HttpContext.Session["auth"]))
+            {
+                // 驗證成功
+            }
+            else
+            {
+                base.HandleUnauthorizedRequest(filterContext);
+            }
+        }
+    }
+}
